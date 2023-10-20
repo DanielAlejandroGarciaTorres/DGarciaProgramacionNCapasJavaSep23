af740202bcf42ca0587f086072300b8dd0b7f4dc
DanielAlejandroGarciaTorres <dgarciatorres97@gmail.com>
2023-10-20 13:19:53 -0600

    Carga Masiva

--- src/main/java/com/digis01/DGarciaProgramacionNCapasWeb/controller/CargaMasiva.java
+++ src/main/java/com/digis01/DGarciaProgramacionNCapasWeb/controller/CargaMasiva.java
@@ -5,7 +5,11 @@
 package com.digis01.DGarciaProgramacionNCapasWeb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -16,6 +20,18 @@ public class CargaMasiva {
     
     @GetMapping("/CargaMasiva")
     public String CargaMasiva(){
+        
+        return "CargaMasiva";
+    }
+    
+    @PostMapping("/CargaMasiva/excel")
+    public String CargaMasivaExel(@RequestParam MultipartFile archivoExcel) {
+        
+        return "CargaMasiva";
+    }
+    @PostMapping("/CargaMasiva/txt")
+    public String CargaMasivaTxt(@RequestParam MultipartFile archivoTxt) {
+        
         return "CargaMasiva";
     }
 }
